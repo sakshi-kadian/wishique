66aa55d1f801a71b19e024e9cec66e3b603cc313
sakshi-kadian <kadiansakshi.sk@gmail.com>
2025-08-10 17:58:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://wishique.onrender.com" // your deployed frontend URL
+      "https://wishique.onrender.com" 
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
